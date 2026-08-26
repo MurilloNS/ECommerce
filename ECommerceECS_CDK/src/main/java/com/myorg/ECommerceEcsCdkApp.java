216f219fe90459c219a6f9aec995ceb47921d4a2
Murillo <murillo.murillo2001@gmail.com>
2026-08-26 15:55:23 -0300

    configure environment and ECR repository

--- ECommerceECS_CDK/src/main/java/com/myorg/ECommerceEcsCdkApp.java
+++ ECommerceECS_CDK/src/main/java/com/myorg/ECommerceEcsCdkApp.java
@@ -4,15 +4,29 @@ import software.amazon.awscdk.App;
 import software.amazon.awscdk.Environment;
 import software.amazon.awscdk.StackProps;
 
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ECommerceEcsCdkApp {
     public static void main(final String[] args) {
         App app = new App();
 
+        Environment environment = Environment.builder()
+                .account(System.getenv("CDK_DEFAULT_ACCOUNT"))
+                .region(System.getenv("CDK_DEFAULT_REGION"))
+                .build();
 
+        Map<String, String> infraTags = new HashMap<>();
+        infraTags.put("team", "olirrum");
+        infraTags.put("project", "ECommerce");
+        infraTags.put("environment", "dev");
+        infraTags.put("cost", "ECommerceInfra");
+
+        EcrStack ecrStack = new EcrStack(app, "Ecr", StackProps.builder()
+                .env(environment)
+                .tags(infraTags)
+                .build());
 
         app.synth();
     }
 }
-
