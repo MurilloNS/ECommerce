876ab0f0ee75ae7567c3c32142ea4f1df2978ea5
Murillo <murillo.murillo2001@gmail.com>
2026-09-15 16:52:11 -0300

    Updating ProductController

--- productsservice/src/main/java/br/com/murillo/productsservice/products/controllers/ProductController.java
+++ productsservice/src/main/java/br/com/murillo/productsservice/products/controllers/ProductController.java
@@ -1,19 +1,87 @@
 package br.com.murillo.productsservice.products.controllers;
 
+import br.com.murillo.productsservice.products.dtos.ProductDTO;
+import br.com.murillo.productsservice.products.models.Product;
+import br.com.murillo.productsservice.products.repositories.ProductRepository;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.CompletionException;
 
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
     private static final Logger LOG = LogManager.getLogger(ProductController.class);
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
 
     @GetMapping
-    public String getAllProducts() {
+    public ResponseEntity<List<ProductDTO>> getAllProducts() {
         LOG.info("Get all products");
-        return "All products";
+
+        List<ProductDTO> productsDTO = new ArrayList<>();
+
+        productRepository.getAll().items().subscribe(product ->
+                productsDTO.add(new ProductDTO(product))).join();
+
+        return new ResponseEntity<>(productsDTO, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getProductById(@PathVariable String id) {
+        Product product = productRepository.getById(id).join();
+
+        if (product != null)
+            return new ResponseEntity<>(new ProductDTO(product), HttpStatus.OK);
+        else
+            return new ResponseEntity<>("Product not found!", HttpStatus.NO_CONTENT);
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductDTO> createProduct(@RequestBody ProductDTO productDTO) {
+        Product productCreated = ProductDTO.toProduct(productDTO);
+        productCreated.setId(UUID.randomUUID().toString());
+
+        productRepository.create(productCreated).join();
+
+        LOG.info("Product created - ID: {}", productCreated.getId());
+
+        return new ResponseEntity<>(new ProductDTO(productCreated), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteProductById(@PathVariable String id) {
+        Product productDeleted = productRepository.deleteById(id).join();
+
+        if (productDeleted != null) {
+            LOG.info("Product deleted - ID: {}", productDeleted.getId());
+            return new ResponseEntity<>(new ProductDTO(productDeleted), HttpStatus.OK);
+        }
+        else
+            return new ResponseEntity<>("Product not found!", HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateProduct(@PathVariable String id, @RequestBody ProductDTO productDTO) {
+        try {
+            Product productUpdated = productRepository.update(ProductDTO.toProduct(productDTO), id).join();
+
+            LOG.info("Product updated - ID: {}", productUpdated.getId());
+
+            return new ResponseEntity<>(new ProductDTO(productUpdated), HttpStatus.OK);
+        } catch (CompletionException e) {
+            return new ResponseEntity<>("Product not found!", HttpStatus.NO_CONTENT);
+        }
     }
 }
--- productsservice/src/main/java/br/com/murillo/productsservice/products/dtos/ProductDTO.java
+++ productsservice/src/main/java/br/com/murillo/productsservice/products/dtos/ProductDTO.java
@@ -1,10 +1,13 @@
 package br.com.murillo.productsservice.products.dtos;
 
 import br.com.murillo.productsservice.products.models.Product;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
-public record ProductDTO(String id, String name, String code, float price, String model) {
+public record ProductDTO(String id, String name, String code, float price, String model,
+                         @JsonInclude(JsonInclude.Include.NON_NULL) String url) {
     public ProductDTO(Product product) {
-        this(product.getId(), product.getProductName(), product.getCode(), product.getPrice(), product.getModel());
+        this(product.getId(), product.getProductName(), product.getCode(), product.getPrice(), product.getModel(),
+                product.getProductUrl());
     }
 
     static public Product toProduct(ProductDTO productDTO) {
@@ -14,6 +17,7 @@ public record ProductDTO(String id, String name, String code, float price, Strin
         product.setCode(productDTO.code);
         product.setPrice(productDTO.price);
         product.setModel(productDTO.model);
+        product.setProductUrl(productDTO.url());
 
         return product;
     }
